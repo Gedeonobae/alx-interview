d40d5778d091421778e627a213d6415b16cfec26
Gedeon Obae Gekonge <85337093+Gedeonobae@users.noreply.github.com>
2022-11-14 02:41:02 +0300

    upload 0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""`canUnlockAll` module
+"""
+
+
+def canUnlockAll(boxes):
+    """A method that determines if all `boxes` can be opened
+        @boxes: A list of list of positive ints
+        rtype: `True` if all boxes can be opened, else `False`
+    """
+    if not boxes or type(boxes) is not list:
+        return False
+    unlocked_boxes = [0]
+    for i in unlocked_boxes:
+        for key in boxes[i]:
+            if key not in unlocked_boxes and key < len(boxes):
+                unlocked_boxes.append(key)
+    return len(unlocked_boxes) == len(boxes)
